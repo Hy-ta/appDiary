98d8c8e9aa799fa01c0a9ff1408c310d7b42b142
Hy-ta <hyuuga.1.29.59@gamil.com>
2021-01-28 17:10:29 +0900

    change in procfile

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{path?}', 'app');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/{any}', function(){
+//     return view('app');
+// })->where('any', '.*');
+
+// Route::view('/{path?}', 'app');
+
+
+Route::get('/', function () {
+     return View('app'); 
+    });
+
+Route::group(['prefix' => 'api'], function () {
+    Route::get('me', 'UserController@me');
+});
