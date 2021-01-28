debe1f3434cb703221396abdca1898b750558ed3
Hy-ta <hyuuga.1.29.59@gamil.com>
2021-01-28 12:03:04 +0900

    change in procfile

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/{any}', function(){
-//     return view('app');
-// })->where('any', '.*');
 Route::view('/{path?}', 'app');
