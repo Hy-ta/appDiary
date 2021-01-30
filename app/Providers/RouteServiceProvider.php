b78044e925ef7c88cbf156101d6f42f940d33b47
Hy-ta <hyuuga.1.29.59@gamil.com>
2021-01-30 11:57:28 +0900

    d

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DiaryController;
+
 
 class RouteServiceProvider extends ServiceProvider
 {
