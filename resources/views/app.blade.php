91cc1852e25e4cf7228087239ccc90f1ba583080
Hy-ta <hyuuga.1.29.59@gamil.com>
2021-02-03 11:29:35 +0900

    table layouts adjust

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,7 +22,8 @@
     <body>
         <div id="app"></div>
         
-        <script src="{{ asset('js/app.js', true) }}"></script>
         <!-- <script src="{{ asset('js/app.js') }}"></script> -->
+        <script src="{{ asset('js/app.js', true) }}"></script>
+
     </body>
 </html>
